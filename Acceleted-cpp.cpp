6f7f2e3f0b7be7c3e26a09709af7bfd7b64ee51d
KEATON <rapier83@gmail.com>
2019-09-25 14:45:40 +0900

    제일 간단한 어플리케이션

--- Acceleted-cpp.cpp
+++ Acceleted-cpp.cpp
@@ -4,39 +4,58 @@
 #include <iostream>
 #include <string>
 
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
 int main()
 {
 
-	std::cout << "Plese Enter some text...";
-	std::string sometext;
-	std::cin >> sometext;
+	cout << "Plese Enter some text...";
+	string sometext;
+	cin >> sometext;
 
 	// output message
-	const std::string greeting = "Hello " + sometext + "!";
+	const string greeting = "Hello " + sometext + "!";
 
-	// row 2, 4
-	const std::string spaces(greeting.size(), ' '); // 상수const를 정의할 때 쉼표로 구분하여 '2개의 표현식을' 괄호로 묶어서 정의할 수 있다. 문자열수 + 1을 정의하려고 이렇게 했음
-	const std::string second = "* " + spaces + " *";
+	const int pad = 1;
+	const int rows = pad * 2 + 3;
+	const string::size_type cols = greeting.size() + pad * 2 + 3;
 
-	// row 1, 5
-	const std::string first(second.size(), '*');
+	cout << endl;
 
-	//write to screen
-	std::cout << std::endl;
-	std::cout << first << std::endl;
-	std::cout << second << std::endl;
-	std::cout << "* " << greeting << " *" << std::endl;
-	std::cout << second << std::endl;
-	std::cout << first << std::endl;
+
+
+	for (int r = 0; r != rows; ++r)
+	{
+		string::size_type c = 0;
+
+		while (c != cols)
+		{
+			if (r == pad + 1 && c == pad + 1)
+			{
+				cout << greeting;
+				c += greeting.size(); // 여기서 루프를 끝낼 지점을 생성함 딱 한번만 실행되어야 함
+			}
+			else
+			{
+				if (r == 0 || r == rows - 1 ||
+					c == 0 || c == cols - 1)
+				{
+					cout << "*";
+				}
+				else
+				{
+					cout << " ";
+				}
+				++c; // 처리가 끝나면 c 를 추가해서 다음 루프로 이어지게 햄
+			}
+		}
+		cout << endl; // while 이 끝날때 마다 - 한 cols가 다 돌 떄 마다 줄바꿈
+
+	}
+	
+	return 0;
+
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
